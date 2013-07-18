84e239c39f1f01d83aaa2c7eb99e3a42b0bd702a
snop_snov <ssaweleva@gmail.com>
2013-07-18 13:18:31 +0000

    modified builder

--- lib/configus.rb
+++ lib/configus.rb
@@ -1,37 +1,12 @@
 require "configus/version"
 
 module Configus
+  require 'configus/builder'
+  require 'configus/config'
 
-  class Builder
-    def self.env(name, &block)
-      conf = Config.new
-      conf.instance_eval(&block)
-      conf
-      #raise "XXXXXXXXXXX" << conf.inspect
-    end
-  end
-
-  class Config < Hash
-    #def get_method_name(name_str)
-    #  name_str[0, name_str.length - 1].to_sym
-    #end
-
-
-    def method_missing(method_name, *args, &block)
-      #method_name = get_method_name(method_name) if method_name =~ /\=$/
-      #tt = block_given?
-#raise "XXXXXXXXXXXXX" << tt.inspect
-
-      if block_given?
-        self[method_name] = Config.new
-      end
-
-      if self.has_key?(method_name)
-        self[method_name]
-      else
-        self[method_name] = args.first
-      end
-    end
-
+  def self.build(env_name, &block)
+    b = Builder.new
+    b.instance_eval(&block)
+    raise "Env does not exist" unless b.has_env? env_name
   end
 end
--- test/configus/configus_test.rb
+++ test/configus/configus_test.rb
@@ -6,12 +6,26 @@ describe "Configus" do
   end
 
   it "simple config data" do
-    c = Configus::Builder.env :production do
+    c = Configus::Builder.new
+    conf = c.env :production do
       website_url 'http://example.com'
     end
-    c.website_url.must_equal 'http://example.com'
+    conf.website_url.must_equal 'http://example.com'
   end
 
+  it "check unexisted environment" do
+    lambda do
+      Configus.build :user_environment do
+        env :production do
+          website_url 'http://example.com'
+          email do
+          end
+        end
+      end
+    end.must_raise(RuntimeError)
+  end
+
+=begin
   it "nested one level" do
     c = Configus::Builder.env :production do
       website_url 'http://example.com'
@@ -31,4 +45,24 @@ describe "Configus" do
     c.email.address.must_equal 'abc@mail.ru'
   end
 
+  it "nested two level" do
+    c = Configus::Builder.env :production do
+      website_url 'http://example.com'
+      email do
+        pop do
+          address 'pop.example.com'
+          port    110
+        end
+        smtp do
+          address 'smtp.example.com'
+          port    25
+        end
+      end
+    end
+    #raise "XXXXXXXXXXXXX" << c.inspect
+    c.must_equal({:website_url=>"http://example.com",
+                  :email=>{:pop=>{:address=>"pop.example.com", :port=>110},
+                           :smtp=>{:address=>"smtp.example.com", :port=>25}}})
+  end
+=end
 end
